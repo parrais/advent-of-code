8ac692efc6f4367f2c8ce1a80e9a0987a279e14b
parrais <github@parrais.co.uk>
2025-05-31 00:15:39 +0100

    Solving 2024 day 3 (part 2)

--- 2024/day03.js
+++ 2024/day03.js
@@ -15,4 +15,22 @@ function mulUncorrupted() {
   return mulTotal;
 }
 
+function mulDoDontUncorrupted() {
+  doDontData = "do()" + data + "don't()";
+  doDontRegex = /do\(\).*?don't\(\)/gs;
+  const mulRegex = /mul\(\d+,\d+\)/g;
+  const doDontArray = doDontData.match(doDontRegex);
+  const mulArray = doDontArray.map((string) => string.match(mulRegex)).flat();
+  let mulTotal = 0;
+  for (const calculation of mulArray) {
+    const comma = calculation.indexOf(",");
+    const firstNum = calculation.slice(4, comma);
+    const secondNum = calculation.slice(comma + 1, -1);
+    mulTotal += firstNum * secondNum;
+  }
+  console.log(mulTotal);
+  return mulTotal;
+}
+
 mulUncorrupted();
+mulDoDontUncorrupted();
